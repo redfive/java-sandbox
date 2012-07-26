30822cf39ff8cf8e87fef79b66cc79cb26bd0c04
John (redfive) Gaunt <redfive@songbirdnest.com>
2012-07-26 15:32:40 -0700

    Completed the string intersection problem. - translate from a map->set->iterator and then check for overlap - add comments - added arg checking so the strings can be passed in on the CLI - split the function out of main and into it's own static function

--- src/com/jgaunt/java/samples/stringintersection/Main.java
+++ src/com/jgaunt/java/samples/stringintersection/Main.java
@@ -3,18 +3,26 @@ package com.jgaunt.java.samples.stringintersection;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.lang.Character;
 import java.lang.Integer;
 
+/**
+ * Purpose of this class is to find the character with the greatest intersection
+ *   between two Strings. 
+ */
 public class Main {
-    public static void main (String [] args) {
-        String stringA = "aabbccdd";
-        String stringB = "abcdd";
+    public static Character findIntersection ( String aStringA, String aStringB) {
 
+        // store the counts for each character for each string
         HashMap <Character, Integer> mapA = new HashMap<Character, Integer>();
         HashMap <Character, Integer> mapB = new HashMap<Character, Integer>();
 
-        StringCharacterIterator iterA = new StringCharacterIterator(stringA);
+        // build up the map for stringA
+        StringCharacterIterator iterA = new StringCharacterIterator(aStringA);
         for(char c = iterA.first(); c != CharacterIterator.DONE; c = iterA.next()) {
             Character cchar = new Character(c);
             if (mapA.containsKey(cchar)) {
@@ -24,9 +32,9 @@ public class Main {
                 mapA.put(cchar, new Integer(1));
             }
         }
-        System.out.println(mapA);
 
-        StringCharacterIterator iterB = new StringCharacterIterator(stringB);
+        // build up the map for stringB
+        StringCharacterIterator iterB = new StringCharacterIterator(aStringB);
         for(char c = iterB.first(); c != CharacterIterator.DONE; c = iterB.next()) {
             Character cchar = new Character(c);
             if (mapB.containsKey(cchar)) {
@@ -36,7 +44,52 @@ public class Main {
                 mapB.put(cchar, new Integer(1));
             }
         }
-        System.out.println(mapB);
 
+        // the two HashMaps now have counts for each letter contained in the input strings
+
+        // cash the current intersection character and it's count
+        Character maxChar = ' ';
+        Integer maxCount = 0;
+
+        // Iterate over the first string's map and compare that to the second map
+        //   directly to avoid an O(n^2) operation. Another optimization here would
+        //   be to pick the shorter of the two maps but given that at most we're 
+        //   talking about 26 characters, for this problem not a big gain.
+        Set <Map.Entry<Character, Integer>> setA = mapA.entrySet();
+        Iterator <Map.Entry<Character, Integer>> setIterA = setA.iterator();
+        while ( setIterA.hasNext() ) {
+            Map.Entry<Character, Integer> mapAEntry = setIterA.next();
+
+            // look up the values only once
+            Character curChar = mapAEntry.getKey();
+            Integer curCount = mapAEntry.getValue();
+            if ( mapB.containsKey( curChar ) ) {
+                // There is an intersection now, the value will be the smaller
+                // of the two counts.
+                Integer intersectionCount = 0;
+                Integer countB = mapB.get(curChar);
+                if ( curCount < countB ) {
+                    intersectionCount = curCount;
+                } else {
+                    intersectionCount = countB;
+                }
+
+                // Check to see if this intersection is bigger. Ties go to the
+                // first one found.
+                if (intersectionCount > maxCount) {
+                    maxCount = intersectionCount;
+                    maxChar = curChar;
+                }
+            }
+        }
+        return maxChar;
+    }
+
+    public static void main (String [] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: com.jgaunt.java.samples.stringintersection.main <string> <string>");
+            return;
+        }
+        System.out.println( findIntersection(args[0], args[1]) );
     }
 }
